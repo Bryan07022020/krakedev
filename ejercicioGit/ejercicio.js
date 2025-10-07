9c4a9067889b5983c82cbb29d9fa09938ce32f97
Bryan07022020 <seguridad.informatica.bc@gmail.com>
2025-10-07 16:18:37 -0500

    el boton modificar la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,4 +1,8 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    let imagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/homero2.gif"
     console.log("Ingresa a saludar");
-    
+
 }
