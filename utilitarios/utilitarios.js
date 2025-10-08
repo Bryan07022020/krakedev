a0cebfb2f30a05e4fbe35ca47c04a0304be346e9
Bryan07022020 <seguridad.informatica.bc@gmail.com>
2025-10-07 22:44:09 -0500

    metodo funcion recuperar texto ok

---
+++ utilitarios/utilitarios.js
@@ -0,0 +1,16 @@
+saludar=function(){
+    let nombre;
+    nombre=recuperarTexto("txtNombre");
+    let apellido=recuperarTexto("txtApellido");
+}
+
+
+
+
+recuperarTexto=function(idComponente){
+    let componente;
+    let valorIngresado;
+    componente=document.getElementById(idComponente);
+    valorIngresado=componente.value;
+    return valorIngresado;
+}
