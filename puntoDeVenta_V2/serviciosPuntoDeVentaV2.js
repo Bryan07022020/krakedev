db885aecc6ab178cd781fdc754a034e7556763e9
Bryan07022020 <seguridad.informatica.bc@gmail.com>
2025-10-20 16:44:03 -0500

    Se guarda la calculadora y se crea los archivos punto de venta

---
+++ puntoDeVenta_V2/serviciosPuntoDeVentaV2.js
@@ -0,0 +1,19 @@
+calcularValorDescuento=function(precioProducto,porcentajeDescuento){
+    let valorDescuento=precioProducto*porcentajeDescuento/100;
+    return valorDescuento;
+}
+
+calcularIva=function(precioProducto){
+    let valorIVA=precioProducto*0.12;
+    return valorIVA;
+}
+
+calcularSubtotal=function(precioProducto,cantidad){
+    let valorSubtotal=precioProducto*cantidad;
+    return valorSubtotal;
+}
+
+calcularTotal=function(valorSubtotal,valorDescuento,valorIVA){
+    let valorTotal=valorSubtotal-valorDescuento+valorIVA;
+    return valorTotal; 
+}
