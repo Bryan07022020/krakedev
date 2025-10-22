88101dd720751743e745a7636ab1e65adaada3f8
Bryan07022020 <seguridad.informatica.bc@gmail.com>
2025-10-22 17:53:32 -0500

    Se integra la funcion mostrarImpares del 1 al 21

---
+++ bucles/ejemplosFor.js
@@ -0,0 +1,54 @@
+mostrarNumeros=function(){
+    console.log("Antes del For");
+    for(let i=0;i<4;i++){
+    console.log(i)  
+    }
+
+    console.log("Despues del For")
+}
+mostrarNumeros2=function(){
+ console.log("Antes del For");   
+for(let indice=1;indice<=5;indice++){
+    console.log(indice);
+    }
+
+
+ console.log("Despues del For");
+}
+
+mostrarPares=function(){
+  console.log("Antes del For");
+  for(let pares=2;pares<=10;pares+=2){
+    console.log(pares);
+   
+}
+console.log("Despues del For");
+}
+
+mostrarInverso=function(){
+    console.log("Antes del For");
+    for(let y=10;y>=2;y--){
+        console.log(y);
+
+    }
+    console.log("Despues del For");
+}
+
+hackearNasaPelis=function(){
+    //hackeando nasa 0%....100%
+    for(porcentaje=0;porcentaje<=100;porcentaje+=10){
+        console.log(" hackeando Nasa " +porcentaje+ "% ");
+        
+    } 
+    console.log(" LA NASA HA SIDO HACKEADA ");
+}
+
+mostrarImpares=function(){
+    console.log(" antes del For ");
+    for(impar=1;impar<=21;impar+=2){
+    console.log(impar);
+    }
+    console.log("Despues del For");
+}
+
+
