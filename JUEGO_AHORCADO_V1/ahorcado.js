2e730351d9f327bfc144ea4feec002ddcaf673c0
Bryan07022020 <seguridad.informatica.bc@gmail.com>
2025-10-22 23:13:18 -0500

    Se realiza paso uno

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,1 +1,24 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+esMayuscula = function(caracter){
+return (caracter.charCodeAt(0) >= 65 && caracter.charCodeAt(0) <= 90);
+}
+
+esMinuscula = function(caracter){
+return (caracter.charCodeAt(0) >= 97 && caracter.charCodeAt(0) <= 122);
+}
+
+
+let palabraSecreta = ""
+
+guardarPalabra = function(){
+    let palabra = recuperarTexto ("txtSecreta");
+    let mensaje = validacionPalabra(palabra);
+
+    if (mensaje == ""){
+        palabraSecreta = palabra;
+        console.log("Palabra secreta guardada:", palabraSecreta);
+    } else {
+        alert(mensaje);
+    }
+
+}
