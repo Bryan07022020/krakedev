09e3fec2e024940f9e3894b489cbb998cdb2e79c
Bryan07022020 <seguridad.informatica.bc@gmail.com>
2025-10-29 12:19:07 -0500

    Se genera los arreglos y carpetas nuevas

---
+++ bucles/autoevaluacion.js
@@ -0,0 +1,84 @@
+mostrarNumeros=function(){
+    console.log("Antes del For");
+    for(let i=1; i<=5; i ++){
+    console.log(i)  
+    }
+
+    console.log("Despues del For")
+}
+mostrarNumeros2=function(){
+ console.log("Antes del For");   
+for(let indice=5; indice<=10; indice++){
+    console.log(indice);
+    }
+
+
+ console.log("Despues del For");
+}
+
+mostrarNumeros3=function(){
+ console.log("Antes del For");   
+for(let i=0; i<=10; i ++){
+    let imprimir= i + 2;
+    console.log(imprimir);
+    }
+ console.log("Despues del For");
+}
+
+mostrarNumeros4=function(){
+ console.log("Antes del For");   
+for(let i=9; i>0; i --){
+    imprimir= i - 1;
+    console.log(imprimir);
+    }
+ console.log("Despues del For");
+}
+
+mostrarNumeros5=function(){
+ console.log("Antes del For");   
+for(let i=5; i>0; i --){
+    imprimir= i + 2;
+    console.log(imprimir);
+    }
+ console.log("Despues del For");
+}
+
+mostrarNumeros6=function(){
+    let par=0;
+for(let i=2; i<=10; i+=2){
+    console.log("pares",i);
+    par+= i;
+    console.log(par);
+    }
+ console.log("suma de todos los pares: ",par);
+}
+
+mostrarNumeros7=function(){
+for(let i=5; i<=40; i+=5){
+    
+    console.log(i);
+    }
+ console.log("despues");
+}
+
+mostrarNumeros8=function(){
+    console.log("Antes del for");
+
+    for(let i=0; i<0; i++){
+    
+    console.log(i);
+    }
+    console.log("despues del for ");
+}
+
+mostrarNumeros9=function(){
+    console.log("Antes del for");
+    for(let i=0; i<=0; i++){
+    
+    console.log(i);
+    }
+    console.log("despues del for ");
+}
+
+
+
